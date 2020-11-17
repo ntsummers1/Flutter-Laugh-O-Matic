075fc230081254d8535640b4765473a0b2874644
summer68 <summer68@msu.edu>
2020-11-16 23:30:02 -0500

    Setup json file reading for test

--- test/features/joke/data/models/joke_model_test.dart
+++ test/features/joke/data/models/joke_model_test.dart
@@ -1,16 +1,30 @@
+import 'dart:convert';
+
 import 'package:flutter_test/flutter_test.dart';
 import 'package:myapp/features/joke/data/models/joke_model.dart';
 import 'package:myapp/features/joke/domain/entities/joke.dart';
 
+import '../../../../core/fixtures/fixture_reader.dart';
+
 void main() {
-  final jokeModel = JokeModel(
+  final tJokeModel = JokeModel(
       setup: "What's the best thing about a Boolean?",
       punchline: "Even if you're wrong, you're only off by a bit.");
 
   test(
     'Should be a subclass of Joke Entity',
     () async {
-      expect(jokeModel, isA<Joke>());
+      expect(tJokeModel, isA<Joke>());
     },
   );
+
+  group('fromJson', () {
+    test('Should return a valid model of Joke from JSON', () async {
+      final Map<String, dynamic> jsonMap = json.decode(fixture('json.dart'));
+
+      final result = JokeModel.fromJson(jsonMap);
+
+      expect(result, tJokeModel);
+    });
+  });
 }
